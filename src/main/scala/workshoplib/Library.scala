3ca8955832f272df2287535ec898012776b421c4
msunik <martin.sunik@cloudfarms.com>
2025-04-15 11:20:33 +0200

    The Library was moved to package

--- src/main/scala/workshoplib/Library.scala
+++ src/main/scala/workshoplib/Library.scala
@@ -1,3 +1,5 @@
+package workshoplib
+
 object Library {
   def threeTimes(s: String): String = s * 3
 }
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,3 +1,5 @@
+import workshoplib.Library
+
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
